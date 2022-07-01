ce4e17c3a40f01074ccc30cf0e2879d008952a25
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2022-07-01 10:32:24 +0300

    Heroku Integration

--- dist/main.js
+++ dist/main.js
@@ -10,7 +10,7 @@ var server = new apollo_server_1.ApolloServer({
     typeDefs: schema_1.default,
     resolvers: resolvers_1.default
 });
-server.listen().then(function (_a) {
+server.listen({ port: process.env.PORT || 6000 }).then(function (_a) {
     var url = _a.url;
     console.log("\uD83D\uDE80  Server ready at ".concat(url));
 });
